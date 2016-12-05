64439a8c8034c9ebf4bccd373cc78a91612a6ede
Chris Stair <ccstair@gmail.com>
2016-12-05 18:05:04 -0500

    got jobs data rendering

--- client/formController.js
+++ client/formController.js
@@ -1,16 +1,40 @@
 angular.module('Form', [])
 .controller('FormController', function($scope, HttpService){
   $scope.data = {};
+  $scope.role = {};
+  $scope.results = {};
+  $scope.job = {};
 
   $scope.submitForm = function() {
- 
 
-    HttpService.postData({data: $scope.data})
 
+    HttpService.postData({"data": $scope.data})
       .then(function(resp) {
-   
+
       })
     }
+
+    $scope.getJobData = function() {
+      HttpService.getData()
+        .then(function(res){
+          console.log("this is res", res)
+        $scope.results = res.data;
+        })
+
+            // console.log("result[0].data.jobDescription", res.data)
+    }
+
+    // $scope.getSingleJob = function($scope.role) {
+    //   console.log($scope.role);
+    //
+    //   HttpService.getJob({"role": $scope.role})
+    //     .then(function(resp) {
+    //       console.log(resp)
+    //       $scope.results = resp.data;
+    //     })
+    //
+    // }
+
   })
 .factory('HttpService', function($http){
   let postData = function(data) {
@@ -18,11 +42,31 @@ angular.module('Form', [])
     return $http.post('/form', data)
     .then(function(resp) {
 
-    
+
       return resp
     })
   }
+
+  let getData = function() {
+    return $http.get('/form')
+      .then(function(res){
+        return res
+      })
+
+  }
+
+//   let getJob = function(role) {
+//   //vvvv this only takes one param here
+//   return $http.get('/form/' + role)
+//   .then(function(resp){
+//     return resp;
+//   })
+// }
+
+
   return {
-    postData: postData
+    postData: postData,
+    getData: getData,
+    // getJob: getJob
   }
 })
